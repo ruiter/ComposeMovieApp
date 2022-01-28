ecb484f5ba7be0b988e371969507df179cbf36a6
Ruiter Matos <ruitercomp@gmail.com>
2022-01-28 17:10:59 -0300

    add a lazy column to make a list

--- app/src/main/java/com/ruiter/composemovieapp/MainActivity.kt
+++ app/src/main/java/com/ruiter/composemovieapp/MainActivity.kt
@@ -3,9 +3,14 @@ package com.ruiter.composemovieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,9 +42,54 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MainContent() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Hello")
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Matrix",
+        "Star wars",
+        "Avangers",
+        "Iron Man",
+        "Altered Carbon",
+        "Spider Man",
+        "Captain America",
+        "Harry Potter",
+        "Life"
+    )
+) {
+    Column() {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .height(100.dp),
+        elevation = 4.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                elevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+
+            Text(text = movie)
+        }
     }
 }
 
