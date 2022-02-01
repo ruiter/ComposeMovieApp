440bde69a5bbc4767ada795c9a66acc50b2452b8
Ruiter Matos <ruitercomp@gmail.com>
2022-02-01 10:14:31 -0300

    just add a click event in the card

--- app/src/main/java/com/ruiter/composemovieapp/MainActivity.kt
+++ app/src/main/java/com/ruiter/composemovieapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ruiter.composemovieapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -60,19 +62,24 @@ fun MainContent(
     Column() {
         LazyColumn {
             items(items = movieList) {
-                MovieRow(movie = it)
+                MovieRow(movie = it) { movie ->
+                    Log.i("ruiter", "MainContent: $movie")
+                }
             }
         }
     }
 }
 
 @Composable
-fun MovieRow(movie: String) {
+fun MovieRow(movie: String, onItemClick: (String) -> Unit) {
     Card(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
-            .height(100.dp),
+            .height(100.dp)
+            .clickable {
+                onItemClick(movie)
+            },
         elevation = 4.dp
     ) {
         Row(
