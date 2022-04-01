5edd5e9ed1d217c72060e1b1de73844ea2aac947
Ruiter <ruitercomp@gmail.com>
2022-04-01 15:03:20 -0300

    add a back button on details screen

--- app/src/main/java/com/ruiter/composemovieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/ruiter/composemovieapp/screens/details/DetailsScreen.kt
@@ -1,15 +1,15 @@
 package com.ruiter.composemovieapp.screens.details
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import android.widget.Space
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -22,6 +22,14 @@ fun DetailsScreen(navController: NavController, movie: String?) {
             verticalArrangement = Arrangement.Center
         ) {
             Text(text = movie.toString(), style = MaterialTheme.typography.h4)
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "Go back")
+            }
         }
     }
 }
