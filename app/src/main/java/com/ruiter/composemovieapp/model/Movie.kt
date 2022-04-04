a2d418dba1a9fcd365179bd7a075ddbac98d519b
Ruiter <ruitercomp@gmail.com>
2022-04-04 10:23:55 -0300

    handle with image data requests

--- app/src/main/java/com/ruiter/composemovieapp/model/Movie.kt
+++ app/src/main/java/com/ruiter/composemovieapp/model/Movie.kt
@@ -22,7 +22,7 @@ fun getMovies() = listOf<Movie>(
         director = "Tim Burton",
         actors = "Alec Baldwin, Geena Davis, Annie McEnroe, Maurice Page",
         plot = "A couple of recently deceased ghosts contract the services of a  in order to remove the obnoxious new owners of their house.",
-        posterUrl = "https = //images-na.ssl-images-amazon.com/images/M/MV5BMTUwODE3MDE0MV5BMl5BanBnXkFtZTgwNTk1MjI4MzE@._V1_SX300.jpg"
+        posterUrl = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUwODE3MDE0MV5BMl5BanBnXkFtZTgwNTk1MjI4MzE@._V1_SX300.jpg"
     ),
 
     Movie(
@@ -33,7 +33,7 @@ fun getMovies() = listOf<Movie>(
         director = "Francis Ford Coppola",
         actors = "Richard Gere, Gregory Hines, Diane Lane, Lonette McKee",
         plot = "The Cotton Club was a famous night club in Harlem. The story follows the people that visited the club, those that ran it, and is peppered with the Jazz music that made it so famous.",
-        posterUrl = "https = //images-na.ssl-images-amazon.com/images/M/MV5BMTU5ODAyNzA4OV5BMl5BanBnXkFtZTcwNzYwNTIzNA@@._V1_SX300.jpg"
+        posterUrl = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU5ODAyNzA4OV5BMl5BanBnXkFtZTcwNzYwNTIzNA@@._V1_SX300.jpg"
     ),
 
     Movie(
@@ -44,7 +44,7 @@ fun getMovies() = listOf<Movie>(
         director = "Frank Darabont",
         actors = "Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler",
         plot = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-        posterUrl = "https = //images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SX300.jpg"
+        posterUrl = "https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SX300.jpg"
     ),
 
     Movie(
@@ -55,7 +55,7 @@ fun getMovies() = listOf<Movie>(
         director = "Peter Faiman",
         actors = "Paul Hogan, Linda Kozlowski, John Meillon, David Gulpilil",
         plot = "An American reporter goes to the Australian outback to meet an eccentric crocodile poacher and invites him to New York City.",
-        posterUrl = "https = //images-na.ssl-images-amazon.com/images/M/MV5BMTg0MTU1MTg4NF5BMl5BanBnXkFtZTgwMDgzNzYxMTE@._V1_SX300.jpg"
+        posterUrl = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg0MTU1MTg4NF5BMl5BanBnXkFtZTgwMDgzNzYxMTE@._V1_SX300.jpg"
     ),
 
     Movie(
@@ -66,7 +66,7 @@ fun getMovies() = listOf<Movie>(
         director = "Bryan Singer",
         actors = "Tom Cruise, Kenneth Branagh, Bill Nighy, Tom Wilkinson",
         plot = "A dramatization of the 20 July assassination and political coup plot by desperate renegade German Army officers against Hitler during World War II.",
-        posterUrl = "http = //ia.media-imdb.com/images/M/MV5BMTg3Njc2ODEyN15BMl5BanBnXkFtZTcwNTAwMzc3NA@@._V1_SX300.jpg"
+        posterUrl = "http://ia.media-imdb.com/images/M/MV5BMTg3Njc2ODEyN15BMl5BanBnXkFtZTcwNTAwMzc3NA@@._V1_SX300.jpg"
     ),
 
     Movie(
@@ -77,7 +77,7 @@ fun getMovies() = listOf<Movie>(
         director = "Brad Bird, Jan Pinkava",
         actors = "Patton Oswalt, Ian Holm, Lou Romano, Brian Dennehy",
         plot = "A rat who can cook makes an unusual alliance with a young kitchen worker at a famous restaurant.",
-        posterUrl = "https = //images-na.ssl-images-amazon.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_SX300.jpg"
+        posterUrl = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_SX300.jpg"
     ),
 
     Movie(
@@ -88,7 +88,7 @@ fun getMovies() = listOf<Movie>(
         director = "Fernando Meirelles, Kátia Lund",
         actors = "Alexandre Rodrigues, Leandro Firmino, Phellipe Haagensen, Douglas Silva",
         plot = "Two boys growing up in a violent neighborhood of Rio de Janeiro take different paths =  one becomes a photographer, the other a drug dealer.",
-        posterUrl = "https = //images-na.ssl-images-amazon.com/images/M/MV5BMjA4ODQ3ODkzNV5BMl5BanBnXkFtZTYwOTc4NDI3._V1_SX300.jpg"
+        posterUrl = "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA4ODQ3ODkzNV5BMl5BanBnXkFtZTYwOTc4NDI3._V1_SX300.jpg"
     ),
 
     Movie(
@@ -99,7 +99,7 @@ fun getMovies() = listOf<Movie>(
         director = "Christopher Nolan",
         actors = "Guy Pearce, Carrie-Anne Moss, Joe Pantoliano, Mark Boone Junior",
         plot = "A man juggles searching for his wife's murderer and keeping his short-term memory loss from being an obstacle.",
-        posterUrl = "https = //images-na.ssl-images-amazon.com/images/M/MV5BNThiYjM3MzktMDg3Yy00ZWQ3LTk3YWEtN2M0YmNmNWEwYTE3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+        posterUrl = "https://images-na.ssl-images-amazon.com/images/M/MV5BNThiYjM3MzktMDg3Yy00ZWQ3LTk3YWEtN2M0YmNmNWEwYTE3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
     ),
 
     Movie(
@@ -110,7 +110,7 @@ fun getMovies() = listOf<Movie>(
         director = "Olivier Nakache, Eric Toledano",
         actors = "François Cluzet, Omar Sy, Anne Le Ny, Audrey Fleurot",
         plot = "After he becomes a quadriplegic from a paragliding accident, an aristocrat hires a young man from the projects to be his caregiver.",
-        posterUrl = "http = //ia.media-imdb.com/images/M/MV5BMTYxNDA3MDQwNl5BMl5BanBnXkFtZTcwNTU4Mzc1Nw@@._V1_SX300.jpg"
+        posterUrl = "http://ia.media-imdb.com/images/M/MV5BMTYxNDA3MDQwNl5BMl5BanBnXkFtZTcwNTU4Mzc1Nw@@._V1_SX300.jpg"
     ),
 
     Movie(
@@ -121,6 +121,6 @@ fun getMovies() = listOf<Movie>(
         director = "Matthew Vaughn",
         actors = "Ian McKellen, Bimbo Hart, Alastair MacIntosh, David Kelly",
         plot = "In a countryside town bordering on a magical land, a young man makes a promise to his beloved that he'll retrieve a fallen star by venturing into the magical realm.",
-        posterUrl = "https = //images-na.ssl-images-amazon.com/images/M/MV5BMjkyMTE1OTYwNF5BMl5BanBnXkFtZTcwMDIxODYzMw@@._V1_SX300.jpg"
+        posterUrl = "https://images-na.ssl-images-amazon.com/images/M/MV5BMjkyMTE1OTYwNF5BMl5BanBnXkFtZTcwMDIxODYzMw@@._V1_SX300.jpg"
     )
 )
--- app/src/main/java/com/ruiter/composemovieapp/widgets/Widgets.kt
+++ app/src/main/java/com/ruiter/composemovieapp/widgets/Widgets.kt
@@ -1,16 +1,28 @@
 package com.ruiter.composemovieapp.widgets
 
 import Movie
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
+import com.ruiter.composemovieapp.R
 import getMovies
 
 @Preview
@@ -36,7 +48,16 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
                     .size(100.dp),
                 elevation = 4.dp
             ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(movie.posterUrl)
+                        .crossfade(true)
+                        .transformations(CircleCropTransformation())
+                        .build(),
+                    contentDescription = "Movie image",
+                    contentScale = ContentScale.Crop
+                )
+//                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
             }
 
             Column(modifier = Modifier.padding(4.dp)) {
