06c9f5a0a8e57eafe62df1b9fc59566a349b0924
Ruiter <ruitercomp@gmail.com>
2022-04-06 15:45:03 -0300

    add more info on the movie card

--- app/src/main/java/com/ruiter/composemovieapp/widgets/Widgets.kt
+++ app/src/main/java/com/ruiter/composemovieapp/widgets/Widgets.kt
@@ -80,7 +80,7 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
 
                 AnimatedVisibility(visible = expanded) {
                     // hidden info
-                    Column() {
+                    Column {
                         Text(buildAnnotatedString {
                             withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)) {
                                 append("Plot: ")
@@ -90,12 +90,30 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
                                 style = SpanStyle(
                                     color = Color.DarkGray,
                                     fontSize = 13.sp,
-                                    fontWeight = FontWeight.Bold
+                                    fontWeight = FontWeight.Light
                                 )
                             ) {
                                 append(movie.plot)
                             }
-                        })
+                        }, modifier = Modifier.padding(6.dp))
+
+                        Divider(modifier = Modifier.padding(4.dp))
+
+                        Text(
+                            text = "Director: ${movie.director}",
+                            style = MaterialTheme.typography.caption,
+                            fontWeight = FontWeight.Light
+                        )
+                        Text(
+                            text = "Actors: ${movie.actors}",
+                            style = MaterialTheme.typography.caption,
+                            fontWeight = FontWeight.Light
+                        )
+                        Text(
+                            text = "Runtime: ${movie.runtime}",
+                            style = MaterialTheme.typography.caption,
+                            fontWeight = FontWeight.Light
+                        )
                     }
                 }
 
